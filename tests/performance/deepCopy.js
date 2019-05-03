228c90a6ae284be8c5b826e79b16b299430d9658
Walle Cyril <cyril.walle@protonmail.com>
2019-05-04 00:06:18 +0200

    refactor performance test
    make it work
    use leistung package,
    use ES modules

--- tests/performance/deepCopy.js
+++ tests/performance/deepCopy.js
@@ -1,49 +1,44 @@
-"use strict"; // todo make the test work without manual import
-const performanceTestFrameWork = require("./performanceTestFrameWork.js"); // https://github.com/GrosSacASac/JavaScript-Set-Up/blob/master/js/performanceTestFrameWork.js
+import { performance } from "perf_hooks";
+import { createPerformanceTest, runAll } from "leistung";
 
-const deepCopy = require("./deepCopy.js"); // utilsac
+import { deepCopy } from "../../utility.js";
+
+
 const deepCopyJSON = x => JSON.parse(JSON.stringify(x));
 
 const a = {
-    b:7,
+    b: 7,
     c: "",
     d: "ddddd",
     e: {
         f: {},
         g: "g",
-        z: {z: {z: {z: {eq: null, o: -45}}}}
+        z: { z: { z: { z: { eq: null, o: -45 } } } }
     },
-    h: [5,43]
+    h: [5, 43]
 };
 
 let b;
 
-const JSONTest = {
-	name: 'JSON',
-	code: (shared, finish) => {
-            b = deepCopyJSON(a);
-			finish();
-		}
+const JSONCopyTest = {
+    name: 'JSONCopy',
+    code: (shared, finish) => {
+        b = deepCopyJSON(a);
+        finish();
+    }
 };
 
 const deepCopyTest = {
-	name: 'deepCopy',
-	code: (shared, finish) => {
-            b = deepCopy(a);
-			finish();
-		}
+    name: 'deepCopy',
+    code: (shared, finish) => {
+        b = deepCopy(a);
+        finish();
+    }
 };
 
-const ref = {
-	name: 'ref',
-	code: (shared, finish) => {
-            setTimeout(finish, 10);
-		}
-};
-
-const testSuite = performanceTestFrameWork.create({
-	tests: [JSONTest, deepCopyTest, ref],
-	maxTime: 50
+const testSuite = createPerformanceTest({
+    tests: [JSONCopyTest, deepCopyTest],
+    maxTime: 50,
+    nowReferential: performance.now,
 });
-
-performanceTestFrameWork.runAll(testSuite);
+runAll(testSuite);
