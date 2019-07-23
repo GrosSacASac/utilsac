3a2e4aeafa7b59c535f418a8f883d9ce47bc2167
Walle Cyril <cyril.walle@protonmail.com>
2019-07-23 13:42:28 +0200

    Create type-cast.js

---
+++ type-cast.js
@@ -0,0 +1,33 @@
+export {
+    stringFromArrayBuffer,
+    arrayBufferFromBlob,
+    stringFromBlob,
+};
+
+
+const stringFromArrayBuffer = function (arrayBuffer, encoding = "utf-8") {
+    return (new TextDecoder(encoding)).decode(new DataView(arrayBuffer));
+};
+
+const xFromBlob = function (readAs) {
+    /**
+     * @param {Blob | File} blob
+     * @return {Promise}
+     */
+    return function (blob) {
+        return new Promise(function (resolve, reject) {
+            const reader = new FileReader();
+            reader.onload = function (event) {
+                resolve(reader.result);
+            };
+            reader.onerror = function (error) {
+                reject(error);
+            };
+            reader[readAs](blob);
+        });
+    };
+};
+
+const arrayBufferFromBlob = xFromBlob(`readAsArrayBuffer`);
+
+const stringFromBlob = xFromBlob(`readAsText`);
