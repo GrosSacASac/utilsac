722ac64d9ac616fb9d1dc52e028c3334b2bdbb2f
Walle Cyril <cyril.walle@protonmail.com>
2019-08-01 20:52:05 +0200

    more tests

---
+++ tests/specification/memoizeAsStrings.js
@@ -0,0 +1,49 @@
+import test from "ava";
+import { memoizeAsStrings } from "../../utility.js";
+
+
+test(`it should retun a function`, t => {
+    t.is(typeof memoizeAsStrings(Math.max), `function`);
+});
+
+test(`it should not call the function when the parameters are the same`, t => {
+    let called = 0;
+    // just for the test
+    const sideEffectFunction = function () {
+        called += 1;
+    };
+
+    const memoized = memoizeAsStrings(sideEffectFunction);
+    t.is(called, 0);
+    memoized();
+    t.is(called, 1);
+    memoized();
+    t.is(called, 1);
+});
+
+test(`it should call the function when the parameters are not the same`, t => {
+    let called = 0;
+    const sideEffectFunction = function () {
+        called += 1;
+    };
+
+    const memoized = memoizeAsStrings(sideEffectFunction);
+    t.is(called, 0);
+    memoized(100);
+    t.is(called, 1);
+    memoized(200);
+    t.is(called, 2);
+});
+
+test(`it should return the same when when the parameters are the same`, t => {
+    const sideEffectFunction = function () {
+        return {};
+    };
+
+    const memoized = memoizeAsStrings(sideEffectFunction);
+    const result1 = memoized(100);
+    const result2 = memoized(200);
+    const result3 = memoized(100);
+    t.is(result1, result3);
+    t.not(result1, result2);
+});
