730a392ca5f6256d7793ca3f5d6da8afa3a23b03
Walle Cyril <cyril.walle@protonmail.com>
2019-12-18 16:38:04 +0100

    remove duplicate better naming

--- tests/specification/deepEqual.js
+++ tests/specification/deepEqual.js
@@ -1,6 +1,7 @@
 import test from "ava";
 import { deepEqual, deepEqualAdded } from "../../deep.js";
 
+
 const runBaselineDeepEqualTests = (deepEqualImplementation) => {
     const { name } = deepEqualImplementation;
     test(`${name} should pass if objects have the same contents`, t => {
@@ -115,75 +116,6 @@ const runBaselineDeepEqualTests = (deepEqualImplementation) => {
         t.is(deepEqualImplementation(obj1, obj2), true);
     });
 
-    test(`${name} should pass sample json`, t => {
-        const obj1 = {
-            "product": `Live JSON generator`,
-            "version": 3.1,
-            "releaseDate": `2014-06-25T00:00:00.000Z`,
-            "demo": true,
-            "person": {
-                "id": 12345,
-                "name": `John Doe`,
-                "phones": {
-                    "home": `800-123-4107`,
-                    "mobile": `877-123-1234`,
-                },
-                "email": [
-                    `jd@example.com`,
-                    `jd@example.org`,
-                ],
-                "dateOfBirth": `1980-01-02T00:00:00.000Z`,
-                "registered": true,
-                "emergencyContacts": [
-                    {
-                        "name": `Jane Doe`,
-                        "phone": `888-555-1212`,
-                        "relationship": `spouse`,
-                    },
-                    {
-                        "name": `Justin Doe`,
-                        "phone": `877-123-1212`,
-                        "relationship": `parent`,
-                    },
-                ],
-            },
-        };
-
-        const obj2 = {
-            "product": `Live JSON generator`,
-            "version": 3.1,
-            "releaseDate": `2014-06-25T00:00:00.000Z`,
-            "demo": true,
-            "person": {
-                "id": 12345,
-                "name": `John Doe`,
-                "phones": {
-                    "home": `800-123-4107`,
-                    "mobile": `877-123-1234`,
-                },
-                "email": [
-                    `jd@example.com`,
-                    `jd@example.org`,
-                ],
-                "dateOfBirth": `1980-01-02T00:00:00.000Z`,
-                "registered": true,
-                "emergencyContacts": [
-                    {
-                        "name": `Jane Doe`,
-                        "phone": `888-555-1212`,
-                        "relationship": `spouse`,
-                    },
-                    {
-                        "name": `Justin Doe`,
-                        "phone": `877-123-1212`,
-                        "relationship": `parent`,
-                    },
-                ],
-            },
-        };
-        t.is(deepEqualImplementation(obj1, obj2), true);
-    });
-
     test(`${name} should handle undefined values`, t => {
         const a = { 'value': undefined };
         const b = { 'value': undefined };
@@ -195,7 +127,7 @@ const runBaselineDeepEqualTests = (deepEqualImplementation) => {
 runBaselineDeepEqualTests(deepEqual);
 runBaselineDeepEqualTests(deepEqualAdded);
 
-test(`it should handle Set`, t => {
+test(`deepEqualAdded should handle Set`, t => {
     const a = new Set([1, 2, 3]);
     const b = new Set([1, 2, 3]);
     const c = new Set([4]);
@@ -203,7 +135,7 @@ test(`it should handle Set`, t => {
     t.is(deepEqualAdded(a, c), false);
 });
 
-test(`it should handle Map`, t => {
+test(`deepEqualAdded should handle Map`, t => {
     const a = new Map([[1, 2], [2, 3]]);
     const b = new Map([[1, 2], [2, 3]]);
     const c = new Map([[4, 8]]);
@@ -211,7 +143,7 @@ test(`it should handle Map`, t => {
     t.is(deepEqualAdded(a, c), false);
 });
 
-test(`it should handle RegExp`, t => {
+test(`deepEqualAdded should handle RegExp`, t => {
     const a = /abc/;
     const b = /abc/;
     const c = /xyz/;
@@ -219,7 +151,7 @@ test(`it should handle RegExp`, t => {
     t.is(deepEqualAdded(a, c), false);
 });
 
-test(`it should handle Dates`, t => {
+test(`deepEqualAdded should handle Dates`, t => {
     const a = new Date();
     const b = new Date();
     const commonTime = 10 ** 12;
@@ -231,7 +163,7 @@ test(`it should handle Dates`, t => {
     t.is(deepEqualAdded(a, c), false);
 });
 
-test(`it should handle typed uint8 Arrays`, t => {
+test(`deepEqualAdded should handle typed uint8 Arrays`, t => {
     const numbers = [1, 4, 8, 10];
     const a = new Uint8Array(numbers);
     const b = new Uint8Array(numbers);
@@ -242,7 +174,7 @@ test(`it should handle typed uint8 Arrays`, t => {
     t.is(deepEqualAdded(a, c), false);
 });
 
-test(`it should handle typed uint16 Arrays`, t => {
+test(`deepEqualAdded should handle typed uint16 Arrays`, t => {
     const numbers = [1, 4, 8];
     const a = new Uint16Array(numbers);
     const b = new Uint16Array(numbers);
@@ -253,25 +185,19 @@ test(`it should handle typed uint16 Arrays`, t => {
     t.is(deepEqualAdded(a, c), false);
 });
 
-test(`it should pass for Date`, t => {
-    const d1 = new Date(`2019-01-01`);
-    const d2 = new Date(`2019-01-01`);
-    t.true(deepEqualAdded(d1, d2));
-});
-
-test(`it should pass for array of Dates`, t => {
+test(`deepEqualAdded should pass for array of Dates`, t => {
     const arr1 = Array.from([new Date(`2019-01-01`),new Date(`2019-01-02`), new Date(`2019-01-03`)]);
     const arr2 = Array.from([new Date(`2019-01-01`),new Date(`2019-01-02`), new Date(`2019-01-03`)]);
     t.true(deepEqualAdded(arr1, arr2));
 });
 
-test(`it should fail for array of Dates given different dates`, t => {
+test(`deepEqualAdded should fail for array of Dates given different dates`, t => {
     const arr1 = Array.from([new Date(`2019-01-01`),new Date(`2019-01-02`), new Date(`2019-01-03`)]);
     const arr2 = Array.from([new Date(`2019-01-02`),new Date(`2019-01-02`), new Date(`2019-01-03`)]);
     t.false(deepEqualAdded(arr1, arr2));
 });
 
-test(`it should pass for Map of Dates`, t => {
+test(`deepEqualAdded should pass for Map of Dates`, t => {
     const a = new Map([
         [`a`, new Date(`2019-01-01`)],
         [`b`, new Date(`2019-01-02`)],
@@ -285,7 +211,7 @@ test(`it should pass for Map of Dates`, t => {
     t.true(deepEqualAdded(a, b));
 });
 
-test(`it should fail for Map of Dates given mismatched data`, t => {
+test(`deepEqualAdded should fail for Map of Dates given mismatched data`, t => {
     const a = new Map([
         [`a`, new Date(`2019-01-01`)],
         [`b`, new Date(`2019-01-02`)],
@@ -299,7 +225,7 @@ test(`it should fail for Map of Dates given mismatched data`, t => {
     t.false(deepEqualAdded(a, b));
 });
 
-test(`it should pass for nested Map of Dates`, t => {
+test(`deepEqualAdded should pass for nested Map of Dates`, t => {
     const a = new Map([
         [`a`, new Date(`2019-01-01`)],
         [`b`, new Map([
@@ -320,5 +246,3 @@ test(`it should pass for nested Map of Dates`, t => {
     ]);
     t.true(deepEqualAdded(a, b));
 });
-
-
