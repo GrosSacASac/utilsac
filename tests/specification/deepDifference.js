5927834e9834e8a1ab06400efb1021ddcce9e330
Raene <crofty.52.bu@gmail.com>
2020-02-29 20:19:39 +0100

    added tset for deepDifference

---
+++ tests/specification/deepDifference.js
@@ -0,0 +1,47 @@
+import test from 'ava';
+import { deepDifference } from '../../deep.js';
+
+test('deep difference should return new object with keys Additions, Removals, Changes', t => {
+    const a = {
+        same: 7,
+        x: 30,
+        w: 20,
+        deep: {},
+    };
+
+    const b = {
+        same: 7,
+        x: 15,
+        z: 10,
+        deep: {
+            deeper: 99,
+        },
+    };
+
+    const result = deepDifference(a, b);
+    t.deepEqual(result, {
+        additions: [
+            {
+                name: ['deep', 'deeper'],
+                value: 99,
+            },
+            {
+                name: ['z'],
+                value: 10,
+            },
+        ],
+        removals: [
+            {
+                name: ['w'],
+                value: 20,
+            },
+        ],
+        changes: [
+            {
+                name: ['x'],
+                oldValue: 30,
+                newValue: 15,
+            },
+        ],
+    });
+});
