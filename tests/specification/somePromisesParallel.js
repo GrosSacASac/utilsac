ebae871c42c19f3835cf88afe99c14b74ba7eeb8
Cyril Walle <cyril.walle@protonmail.com>
2021-10-09 10:24:26 +0200

    add more somePromisesParallel tests

--- tests/specification/somePromisesParallel.js
+++ tests/specification/somePromisesParallel.js
@@ -11,27 +11,34 @@ const rejectingPromiseCreator = () => {
     return Promise.reject(rejectValue);
 };
 
-test(`somePromisesParallel returns a promise`, t => {
-    t.is(typeof somePromisesParallel([promiseCreator], 1).then, `function`);
-});
-
-test(`somePromisesParallel resolves with values Array`, async t => {
-    return somePromisesParallel([promiseCreator, promiseCreator], 1).then(values => {
-        t.truthy(Array.isArray(values));
-        values.forEach(value => {
-            t.is(value, resolveValue);
+[1, 10].forEach(x => {
+    test(`somePromisesParallel (x=${x}) returns a promise`, t => {
+        t.is(typeof somePromisesParallel([promiseCreator], x).then, `function`);
+    });
+    
+    test(`somePromisesParallel (x=${x}) with an empty array returns a promise`, t => {
+        t.is(typeof somePromisesParallel([], x).then, `function`);
+    });
+    
+    test(`somePromisesParallel (x=${x}) resolves with values Array`, async t => {
+        return somePromisesParallel([promiseCreator, promiseCreator], x).then(values => {
+            t.truthy(Array.isArray(values));
+            values.forEach(value => {
+                t.is(value, resolveValue);
+            });
         });
     });
-});
-
-test(`somePromisesParallel reject with first rejecting value`, async t => {
-    return somePromisesParallel([
-        promiseCreator,
-        rejectingPromiseCreator,
-        promiseCreator,
-    ], 1).then((values) => {
-        console.warn(values);
-    }).catch(error => {
-        t.is(error, rejectValue);
+    
+    test(`somePromisesParallel (x=${x}) reject with first rejecting value`, async t => {
+        return somePromisesParallel([
+            promiseCreator,
+            rejectingPromiseCreator,
+            promiseCreator,
+        ], x).then((values) => {
+            console.warn(values);
+        }).catch(error => {
+            t.is(error, rejectValue);
+        });
     });
+    
 });
