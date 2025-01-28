c5d705897292130690e12da1e636ed5f18e3c66c
GrosSacASacs <cyril.walle@protonmail.com>
2025-01-28 18:21:10 +0100

    Update typeCastNode.js

--- typeCastNode.js
+++ typeCastNode.js
@@ -2,6 +2,14 @@ export {
     blobPromiseFromStream,
 };
 
+// import { // also
+//     arrayBuffer as arrayBufferPromiseFromStream,
+//     blob as blobPromiseFromStream,
+//     buffer as bufferPromiseFromStream,
+//     json as jsonPromiseFromStream,
+//     text as textPromiseFromStream,
+// } from "node:stream/consumers"; 
+
 
 const blobPromiseFromStream = (stream, mimeType) => {
     return new Response(stream, {headers: new Headers({'Content-Type':  mimeType})}).blob();
